bbe72e88f98a7334e97538d11d9d7b7cae513dd0
callista <165169858+callistabr@users.noreply.github.com>
2024-03-27 20:18:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-834e2b11-7793-4885-832c-e1e0800db67e/)
 
 
 echo "$curl_output" >> Update.tsx
